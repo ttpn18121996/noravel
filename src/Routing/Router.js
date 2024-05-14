ad261d43f5c871201e6ef3b0a2b5ef82e8808d14
ttpn18121996 <ttpn18121996@gmail.com>
2024-05-14 10:47:27 +0700

    Fix register route

--- src/Routing/Router.js
+++ src/Routing/Router.js
@@ -1,46 +1,64 @@
 import { Router as ExpressRouter } from 'express';
 import Route from './Route.js';
 
-export default (() => {
-  const routeRegistered = {
-    get: [],
-    post: [],
-    put: [],
-    patch: [],
-    delete: [],
-    options: [],
-  };
+export default class Router {
+  constructor() {
+    this.routeRegistered = {
+      get: [],
+      post: [],
+      put: [],
+      patch: [],
+      delete: [],
+      options: [],
+    };
+  }
 
-  const add = (method, uri, action) => {
+  add(method, uri, action) {
     const newRoute = new Route(uri, action);
-    if (routeRegistered?.[method.toLowerCase()]) {
-      routeRegistered[method.toLowerCase()].push(newRoute);
+
+    if (this.routeRegistered?.[method.toLowerCase()]) {
+      this.routeRegistered[method.toLowerCase()].push(newRoute);
     }
 
     return newRoute;
-  };
+  }
 
-  return {
-    add,
-    get: (uri, action) => add('get', uri, action),
-    post: (uri, action) => add('post', uri, action),
-    put: (uri, action) => add('put', uri, action),
-    patch: (uri, action) => add('patch', uri, action),
-    delete: (uri, action) => add('delete', uri, action),
-    options: (uri, action) => add('options', uri, action),
-    routeList: () => routeRegistered,
-    run: () => {
-      const router = ExpressRouter();
-      const routeEntries = Object.entries(routeRegistered);
-      for (const [method, routes] of routeEntries) {
-        for (const route of routes) {
-          const middlewares = route.resolveMiddlewares();
-          middlewares.push((req, res, next) => route.execute(req, res, next));
-          router[method](route.uri, ...middlewares);
-        }
+  get(uri, action) {
+    return this.add('get', uri, action);
+  }
+
+  post(uri, action) {
+    return this.add('post', uri, action);
+  }
+
+  put(uri, action) {
+    return this.add('put', uri, action);
+  }
+
+  patch(uri, action) {
+    return this.add('patch', uri, action);
+  }
+
+  delete(uri, action) {
+    return this.add('delete', uri, action);
+  }
+
+  options(uri, action) {
+    return this.add('options', uri, action);
+  }
+
+  run() {
+    const router = ExpressRouter();
+    const routeEntries = Object.entries(this.routeRegistered);
+
+    for (const [method, routes] of routeEntries) {
+      for (const route of routes) {
+        const middlewares = route.resolveMiddlewares();
+        middlewares.push((req, res, next) => route.execute(req, res, next));
+        router[method](route.uri, ...middlewares);
       }
+    }
 
-      return router;
-    },
-  };
-})();
+    return router;
+  }
+}
